af9d0e825efc2d154fe81bb7d645e66c84db6002
Bani4kataa <122724138+Bani4kaa@users.noreply.github.com>
2024-02-08 22:35:35 +0200

    Created main.py

---
+++ key_to_address/main.py
@@ -0,0 +1,45 @@
+from eth_account import Account
+from web3 import Web3, exceptions
+
+Account.enable_unaudited_hdwallet_features()
+
+infura_api_key = 'api_key'
+
+web3 = Web3(Web3.HTTPProvider(f'https://mainnet.infura.io/v3/{infura_api_key}'))
+
+# ANSI color codes
+GREEN = '\033[92m'
+RED = '\033[91m'
+RESET = '\033[0m'
+
+try:
+    with open('memonics.txt', 'r') as file:
+        for line in file:
+            mnemonic = line.strip()
+
+            if not mnemonic:
+                continue
+
+            try:
+                private_key = Account.from_mnemonic(mnemonic)._private_key.hex()
+
+                account = Account.from_key(private_key)
+                address = account.address
+
+                balance_wei = web3.eth.get_balance(address)
+                balance_eth = balance_wei / 10**18 
+
+                print(GREEN + "Ethereum Address:", address)
+                print("Balance:", balance_eth, "ETH" + RESET)
+
+                if balance_eth > 0:
+                    print("Found an Ethereum address with balance above 0. Stopping the program.")
+                    break
+
+            except exceptions.ValidationError:
+                print(RED + f"Invalid private key for mnemonic" + RESET)
+
+except FileNotFoundError:
+    print("File not found.")
+except Exception as e:
+    print("An error occurred:", e)
